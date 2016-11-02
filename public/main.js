ff8aa2dd51dc6b6065313b668079f9a1c6103940
Arman Hovhannisyan <Arman Hovhannisyan>
2016-11-03 00:24:11 +0400

    Rev.1 bug_count:1

---
+++ public/main.js
@@ -0,0 +1,104 @@
+
+
+	const todolist = $('#todo_ul');
+	const search_text = $('#search_box').val();
+	
+    const draw_list = function(){
+    	const search_text = $('#search_box').val();
+    	$('#todo_ul').html('');
+		$.ajax({
+        url      : "/todos",
+        type     : 'get',
+        dataType : 'json',
+        data     : {
+        	searchtext : search_text
+        },
+        success  : function(data) {
+        	console.log(data);
+				data.items.forEach(function(todoItem){
+				const li = $('<li>'+todoItem.message+'<input type="checkbox"><button class="delete" id='+todoItem.id+'>Kill</button></li>')
+				const input = li.find('input');
+				input.prop('checked', todoItem.completed);
+				input.on('change', function(){
+					todoItem.completed = input.prop('checked');
+					$.ajax({
+       				 url         : "/todos/" + todoItem.id,
+       				 type        : 'put',
+      				  dataType    : 'json',
+      				  data        : JSON.stringify(todoItem),
+       				 contentType : "application/json; charset=utf-8",
+       				 success     : function(data) {
+       				 	
+      				  },
+      				  error       : function(data) {
+       			     alert('Error creating todo');
+      				  }
+   				 });
+
+				});
+				
+				todolist.append(li);
+        	});},
+        error    : function(data) {
+            alert('Error searching');
+        }
+    });
+
+};
+$("#search_button").on('click', function(){
+			draw_list();
+		});
+	
+
+$("#create_button").on('click', function(){
+	const val = $('#create_box').val();
+    $('#create_box').val(''); // clear the textbox
+
+    $.ajax({
+        url         : "/todos",
+        type        : 'post',
+        dataType    : 'json',
+        data        : JSON.stringify({
+            message   : val,
+            completed : false
+        }),
+        contentType : "application/json; charset=utf-8",
+        success     : function(data) {
+           location.reload();
+           draw_list(); // refresh the list (re-run the search query)
+        	
+        },
+        error       : function(data) {
+            alert('Error creating todo');
+        }
+    }); 
+
+});
+
+	draw_list();
+	$("#search_box").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#search_button").click(); //pressing enter on the searchbox will act as a button
+    }
+});
+$("#create_box").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#create_button").click(); //pressing enter on the searchbox will act as a button
+    }
+});
+$(document).ready(function(){
+        $(".delete").click(function(todoItem) {
+             $.ajax({
+        url     : "/todos/" + $(this).prop('id') ,
+        type    : 'delete',
+        success : function(data) {
+          location.reload();
+          draw_list();  // remove the rendering of that item from the UI
+        },
+        error   : function(data) {
+            alert('Error deleting the item');
+        }
+    });
+        });
+});
+
---
+++ server.js
@@ -0,0 +1,126 @@
+"use strict";
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+const originPath = './public';
+let todos = [
+				{
+					id: Math.random() + '',
+					message: "Message 1",
+					completed: false
+				},
+				{
+					id: Math.random() + '',
+					message: "Message 2",
+					completed: false
+				}];
+
+const httpServer = http.createServer(function(req, res){
+	
+	
+ 	const parsedUrl = url.parse(req.url);
+    const parsedQuery = querystring.parse(parsedUrl.query);
+    const method = req.method;
+
+	if(req.url==="/")
+	{
+		req.url="/index.html"; //if req.url emtpy redirect to "/index.html"
+	}
+	 if(method === 'GET') {
+        if(req.url.indexOf('/todos') === 0) {
+            res.setHeader('Content-Type', 'application/json');
+            let localTodos = todos;
+
+            if(parsedQuery.searchtext) {
+                localTodos = localTodos.filter(function(obj) {
+                    return obj.message.indexOf(parsedQuery.searchtext) >= 0;
+                });
+            }
+            return res.end(JSON.stringify({items : localTodos}));
+        }
+    }
+    if(method === 'PUT') {
+    	//console.log(req.url);
+        if(req.url.indexOf('/todos') === 0) {
+            // read the content of the message
+            let body = '';
+            req.on('data', function (chunk) {
+                body += chunk;
+            });
+            req.on('end', function () {
+                let jsonObj = JSON.parse(body); // now that we have the content, convert the JSON into an object
+
+                // find the todo in our todos array and replace it with the new object
+                for(let i = 0; i < todos.length; i++) {
+                	//console.log("todos.id = "+ todos[i].id);
+                	//console.log('jsonObj.id = ' + jsonObj.id);
+                    if(todos[i].id === (jsonObj.id)) { // found the same object
+                    	
+                        todos[i] = jsonObj; // replace the old object with the new object
+                        //console.log('ran');
+                        res.setHeader('Content-Type', 'application/json');
+                        res.statusCode = 200;
+                        return res.end(JSON.stringify(jsonObj));
+                    }
+                } 
+					res.statusCode = 404;
+                	return res.end('Data was not found and can therefore not be updated');
+                
+
+            });
+            return;
+        }
+    }
+    if(method === 'POST') {
+        if(req.url.indexOf('/todos') === 0) {
+
+            // read the content of the message
+            let body = '';
+            req.on('data', function (chunk) {
+                body += chunk;
+            });
+            req.on('end', function () {
+                let jsonObj = JSON.parse(body);  // now that we have the content, convert the JSON into an object
+                jsonObj.id = Math.random() + ''; // assign an id to the new object
+                todos[todos.length] = jsonObj;   // store the new object into our array (our 'database')
+
+                res.setHeader('Content-Type', 'application/json');
+                return res.end(JSON.stringify(jsonObj));
+            });
+            return;
+        }
+    }
+    if(method === 'DELETE') {
+        if(req.url.indexOf('/todos/') === 0) {
+            let id =  req.url.substr(7);
+            for(let i = 0; i < todos.length; i++) {
+                if(id === todos[i].id) {
+                    todos.splice(i, 1);
+                    res.statusCode = 200;
+                    return res.end('Successfully removed');
+                }
+            }
+            res.statusCode = 404;
+            return res.end('Data was not found');
+        }
+    }
+	const fileLocation = path.join(originPath, req.url);
+	//console.log(fileLocation);
+		fs.readFile(fileLocation, function(err, data){
+			if(err){
+				res.writeHead(404, 'File not found');
+				res.write('404: Sorry file not found');
+				return res.end();
+			}	else{
+				res.statusCode = 200;
+				return res.end(data);
+			}
+		});
+	
+
+	});
+
+httpServer.listen(80);
